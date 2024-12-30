97f5c731c6da50fd7bc505fddeb5cce0be9d4123
Fabricio cincunegui <160180627+Fabriwin0@users.noreply.github.com>
2024-12-30 04:20:30 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { QuantumSimulator } from './components/QuantumSim/QuantumSimulator';
+
+function App() {
+  return <QuantumSimulator />;
+}
+
+export default App;
